b0c548f1352728cd060f606f4adb2154a4db6324
Joseph Finnegan <joseph.finnegan@protonmail.com>
2021-04-17 19:32:40 +0200

    adding final runner.

---
+++ ns-3-dev-git/runner_remaining.sh
@@ -0,0 +1,36 @@
+./waf --run "lorawan-example -nNodes=2500 -dr=0 --RngRun=1" &> output_thesis/DR0/adr-2500-0-1.txt &
+./waf --run "lorawan-example -nNodes=2500 -dr=0 --RngRun=2" &> output_thesis/DR0/adr-2500-0-2.txt &
+./waf --run "lorawan-example -nNodes=2500 -dr=0 --RngRun=3" &> output_thesis/DR0/adr-2500-0-3.txt &
+./waf --run "lorawan-example -nNodes=2500 -dr=0 --RngRun=4" &> output_thesis/DR0/adr-2500-0-4.txt &
+./waf --run "lorawan-example -nNodes=2500 -dr=0 --RngRun=5" &> output_thesis/DR0/adr-2500-0-5.txt &
+
+./waf --run "lorawan-example -nNodes=2500 -dr=2 --RngRun=1" &> output_thesis/DR2/adr-2500-2-1.txt &
+./waf --run "lorawan-example -nNodes=2500 -dr=2 --RngRun=2" &> output_thesis/DR2/adr-2500-2-2.txt &
+./waf --run "lorawan-example -nNodes=2500 -dr=2 --RngRun=3" &> output_thesis/DR2/adr-2500-2-3.txt &
+./waf --run "lorawan-example -nNodes=2500 -dr=2 --RngRun=4" &> output_thesis/DR2/adr-2500-2-4.txt &
+./waf --run "lorawan-example -nNodes=2500 -dr=2 --RngRun=5" &> output_thesis/DR2/adr-2500-2-5.txt 
+
+./waf --run "lorawan-example -nNodes=2500 -dr=3 --RngRun=1" &> output_thesis/DR3/adr-2500-3-1.txt &
+./waf --run "lorawan-example -nNodes=2500 -dr=3 --RngRun=2" &> output_thesis/DR3/adr-2500-3-2.txt &
+./waf --run "lorawan-example -nNodes=2500 -dr=3 --RngRun=3" &> output_thesis/DR3/adr-2500-3-3.txt &
+./waf --run "lorawan-example -nNodes=2500 -dr=3 --RngRun=4" &> output_thesis/DR3/adr-2500-3-4.txt &
+./waf --run "lorawan-example -nNodes=2500 -dr=3 --RngRun=5" &> output_thesis/DR3/adr-2500-3-5.txt &
+
+./waf --run "lorawan-example -nNodes=2500 -dr=5 --RngRun=1" &> output_thesis/DR5/adr-2500-5-1.txt &
+./waf --run "lorawan-example -nNodes=2500 -dr=5 --RngRun=2" &> output_thesis/DR5/adr-2500-5-2.txt &
+./waf --run "lorawan-example -nNodes=2500 -dr=5 --RngRun=3" &> output_thesis/DR5/adr-2500-5-3.txt &
+./waf --run "lorawan-example -nNodes=2500 -dr=5 --RngRun=4" &> output_thesis/DR5/adr-2500-5-4.txt &
+./waf --run "lorawan-example -nNodes=2500 -dr=5 --RngRun=5" &> output_thesis/DR5/adr-2500-5-5.txt 
+
+./waf --run "lorawan-example-multi-gws -nNodes=100 -dr=1 --RngRun=2" &> output_thesis/multi-DR1/adr-100-1-2.txt &
+./waf --run "lorawan-example-multi-gws -nNodes=100 -dr=1 --RngRun=4" &> output_thesis/multi-DR1/adr-100-1-4.txt &
+./waf --run "lorawan-example-multi-gws -nNodes=100 -dr=4 --RngRun=2" &> output_thesis/multi-DR4/adr-100-4-2.txt &
+./waf --run "lorawan-example-multi-gws -nNodes=100 -dr=4 --RngRun=3" &> output_thesis/multi-DR4/adr-100-4-3.txt &
+./waf --run "lorawan-example-later -nNodes=500 -dr=1 --RngRun=2" &> output_thesis/later-DR1/adr-500-1-2.txt &
+
+./waf --run "lorawan-example-multi-gws -nNodes=2500 -dr=2 --RngRun=1" &> output_thesis/multi-DR2/adr-2500-2-1.txt &
+./waf --run "lorawan-example-multi-gws -nNodes=2500 -dr=2 --RngRun=2" &> output_thesis/multi-DR2/adr-2500-2-2.txt &
+./waf --run "lorawan-example-multi-gws -nNodes=2500 -dr=2 --RngRun=3" &> output_thesis/multi-DR2/adr-2500-2-3.txt &
+./waf --run "lorawan-example-multi-gws -nNodes=2500 -dr=2 --RngRun=4" &> output_thesis/multi-DR2/adr-2500-2-4.txt &
+./waf --run "lorawan-example-multi-gws -nNodes=2500 -dr=2 --RngRun=5" &> output_thesis/multi-DR2/adr-2500-2-5.txt 
+
